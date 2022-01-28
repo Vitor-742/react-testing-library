b7b37569b995d48b5e437499d69fb0117c9c0177
Vitor Campos <vitorcampos742@gmail.com>
2022-01-28 16:23:48 -0300

    requsito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+test('testa o component about', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/about');
+  const title = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+  expect(title).toBeInTheDocument();
+  const paragraphs = screen.getAllByText(/Pokémons/i, { ignore: '.link' });
+  expect(paragraphs).toHaveLength(2);
+  const image = screen.getByRole('img');
+  expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
